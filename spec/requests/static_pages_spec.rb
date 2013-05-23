1fb754e0569d7eaa318aa1104ef69438ef61d600
Spencer Montgomery <smontgo5@gmail.com>
2013-05-23 15:09:03 -0700

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home page" do
+   
+    it "should have the content 'BeSocial'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('BeSocial')
+    end
+
+    it "should have the title 'Home'" do
+    	visit '/static_pages/home'
+    	expect(page).to have_title("BeSocial | Home")
+    end
+  end
+
+ describe "Help page" do
+
+    it "should have the content 'Help'" do
+  	  visit '/static_pages/help'
+  	  expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+    	visit '/static_pages/help'
+    	expect(page).to have_title("BeSocial | Help")
+    end
+  end
+
+  describe "About page"
+
+    it "should have the content 'About Us'" do
+  	  visit '/static_pages/about'
+  	  expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+    	visit '/static_pages/about'
+    	expect(page).to have_title("BeSocial | About Us")
+    end
+  end
+
+
+
+
+
